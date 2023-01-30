9e6621613ffb9e0fcf7e434c767a3965af01aba9
waleed bakar <waleedbakar@outlook.com>
2023-01-30 14:12:50 +0000

    boolens

---
+++ booleans_and_none.py
@@ -0,0 +1,54 @@
+# Boolean
+
+# True or False
+
+# a = True
+# b = False
+#
+# print(a == b) #false
+# print(a != b) #  true is a not = to b
+# print(a >= b) # True
+# print(a >= True) # True
+# print(b <=False) # True
+
+# print(True and False) # False
+# print(False and True) # False
+# print((False or True) # True
+
+# Booleans are useful for  quickly checking the state of something.
+# The other area booleans are useful are validating data types.
+
+# Booleans with data types
+
+# hi = "hello world!"
+#
+# print(hi.isalpha()) # False
+# print(hi.islower())
+# print(hi.endswith("world"))
+# print(hi.startswith("hello"))
+
+# x = 0
+# y = 10
+# z = -15
+#
+# print(bool(x)) # False 0 is always false
+# print(bool(y))
+# print(bool)(z))
+
+
+# None is a place holder
+
+# nONE == NULL In other Lang
+
+print(bool(None))
+
+x = None # FALSE
+
+print(x == False) # False
+
+# Checking if a variable is None
+
+print(x == None) #d irect comparison- more somplex
+print(x is None) # Best practice for checking if something is none
+
+print(type(x)) # <class 'NoneType'>
